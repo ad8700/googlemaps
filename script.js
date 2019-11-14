8559dab831942ee6ad3bdd6b2ef36a1153496d73
andrew.domenico <andrew.domenico@CPX-5N6RCFTFO6Z.dir.svc.accenture.com>
2019-11-14 09:00:29 -0600

    Updated marker code to dynamically add custom icon
    Moved depricated code to bottom of script.js

--- script.js
+++ script.js
@@ -4,24 +4,7 @@ function initMap() {
 			zoom: 15,
 			center: location
 		});
-/*		var marker = new google.maps.Marker({
-			position: location, 
-			map: map
-			})*/
-		var marker = new google.maps.Marker({
-    		map: map,
-    		position: map.getCenter(),
-		    icon: {
-		        path: google.maps.SymbolPath.CIRCLE,
-		        fillColor: '#00F',
-		        fillOpacity: 0.6,
-		        strokeColor: '#00A',
-		        strokeOpacity: 0.9,
-		        strokeWeight: 1,
-		        scale: 7
-    }
-});
-			
+		
 	var address1 = {
 		info: '660 Waterbrook Dr<br> Irving, TX 75039,<br> Internet Available = Y,<br> Max Internet Speed = 1000<br> \
 		TV Available = Y<br> Phone Available = Y<br>',
@@ -36,22 +19,6 @@ function initMap() {
 		long:-96.933168
 	}
 
-/*	var belmont = {
-		info: '<strong>Chipotle on Belmont</strong><br>\
-					1025 W Belmont Ave<br> Chicago, IL 60657<br>\
-					<a href="https://goo.gl/maps/PHfsWTvgKa92">Get Directions</a>',
-		lat: 41.939670,
-		long: -87.655167
-	};
-
-	var sheridan = {
-		info: '<strong>Chipotle on Sheridan</strong><br>\r\
-					6600 N Sheridan Rd<br> Chicago, IL 60626<br>\
-					<a href="https://goo.gl/maps/QGUrqZPsYp92">Get Directions</a>',
-		lat: 42.002707,
-		long: -87.661236
-	};
-*/
 	var locations = [
       [address1.info, address1.lat, address1.long, 0],
 		[address2.info, address2.lat, address2.long, 0]
@@ -72,6 +39,14 @@ function initMap() {
 	for (i = 0; i < locations.length; i++) {
 		marker = new google.maps.Marker({
 			position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+		    icon: {
+		        path: google.maps.SymbolPath.CIRCLE,
+		        fillColor: '#00F',
+		        fillOpacity: 0.6,
+		        strokeColor: '#00A',
+		        strokeOpacity: 0.9,
+		        strokeWeight: 1,
+		        scale: 7},
 			map: map
 		});
 
@@ -83,3 +58,41 @@ function initMap() {
 		})(marker, i));
 	}
 }
+
+
+/*depricated code*/
+
+/*		var marker = new google.maps.Marker({
+			position: location, 
+			map: map
+			})*/
+/*		var marker = new google.maps.Marker({
+    		map: map,
+    		position: map.getCenter(),
+		    icon: {
+		        path: google.maps.SymbolPath.CIRCLE,
+		        fillColor: '#00F',
+		        fillOpacity: 0.6,
+		        strokeColor: '#00A',
+		        strokeOpacity: 0.9,
+		        strokeWeight: 1,
+		        scale: 7
+    }
+});*/
+
+/*	var belmont = {
+		info: '<strong>Chipotle on Belmont</strong><br>\
+					1025 W Belmont Ave<br> Chicago, IL 60657<br>\
+					<a href="https://goo.gl/maps/PHfsWTvgKa92">Get Directions</a>',
+		lat: 41.939670,
+		long: -87.655167
+	};
+
+	var sheridan = {
+		info: '<strong>Chipotle on Sheridan</strong><br>\r\
+					6600 N Sheridan Rd<br> Chicago, IL 60626<br>\
+					<a href="https://goo.gl/maps/QGUrqZPsYp92">Get Directions</a>',
+		lat: 42.002707,
+		long: -87.661236
+	};
+*/
